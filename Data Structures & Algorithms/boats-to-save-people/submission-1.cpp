b35307e1f1ad3af77db46efd2f305912db8f3e5f
Om Sutariya <omsutariya2006@gmail.com>
2026-07-18 16:13:48 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int low=0,high=people.size()-1;
+        int boats=0;
+        while(low<=high){
+            int sum=people[low]+people[high];
+            if(sum<=limit){
+                low++;
+            }
+            high--;
+            boats++;
+        }    
+        return boats;
+    }
+};
